2eafe27bf50fc614618da46b7c959cf042057411
moriarty <alexander@dal.ca>
2013-11-15 21:25:01 +0100

    added imports inside of ompl_demo __init__.py
    from Kine... import KinematicCarPlanningDemo2D
    
    This avoids from ompl_demo.KinematicCarPlanning import KinematicCarPlanningDemo2D
    or 
    car = KinematicCarPlanning.KinematicCarPlanningDemo2D()
    
    now
    car = ompl_demo.KinematicCarPlanningDemo2D()

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+ompl_demo package
+
+A package containing classes related to a simple Open Motion Planning Library Demo
+"""
+__all__=['KinematicCarPlanning']
+
+from KinematicCarPlanning import KinematicCarPlanningDemo2D
+
